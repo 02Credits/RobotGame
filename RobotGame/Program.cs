f75a8e2d645c238b4a8a2ef65ef00e6d1e982e60
keith <keith@the-simmons.net>
2019-04-27 23:12:56 -0700

    Shared project, test project, and ios

---
+++ RobotGame/Program.cs
@@ -0,0 +1,23 @@
+﻿using Foundation;
+using UIKit;
+using RobotGameShared;
+
+namespace RobotGame {
+    [Register("AppDelegate")]
+    class Program : UIApplicationDelegate {
+        private static Game game;
+
+        internal static void RunGame() {
+            game = new Game();
+            game.Run();
+        }
+
+        static void Main(string[] args) {
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+
+        public override void FinishedLaunching(UIApplication app) {
+            RunGame();
+        }
+    }
+}
---
+++ Shared/Game.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace RobotGameShared {
+    public class Game : Microsoft.Xna.Framework.Game {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SpriteFont font;
+        Random random;
+
+        Color foreground = new Color(255, 176, 0);
+        Color background = new Color(40, 40, 40);
+
+        public Game() {
+            graphics = new GraphicsDeviceManager(this);
+#if !IOS
+            graphics.PreferredBackBufferWidth = 1125 / 2;
+            graphics.PreferredBackBufferHeight = 2436 / 2;
+            this.IsMouseVisible = true;
+#endif
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize() {
+            random = new Random();
+            base.Initialize();
+        }
+
+        protected override void LoadContent() {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            font = Content.Load<SpriteFont>("Gugi");
+        }
+
+        protected override void Update(GameTime gameTime) {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime) {
+            graphics.GraphicsDevice.Clear(background);
+            spriteBatch.Begin();
+            spriteBatch.DrawString(font, $"This is a random number: {random.Next() * 100000}", new Vector2(100, 100), foreground);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
+
